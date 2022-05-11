10f14c2d4a53a201592cdd3d1eabdcc1fe45a56d
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-11 17:59:32 +0300

    Edit the Styling of mobile Menu

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const navLogo = document.querySelector('.nav-logo');
+const home = document.querySelector('.home-container-item');
 
 // eslint-disable-next-line no-use-before-define
 hamburger.addEventListener('click', mobileMenu);
@@ -7,6 +9,8 @@ hamburger.addEventListener('click', mobileMenu);
 function mobileMenu() {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  navLogo.classList.toggle('active');
+  home.classList.toggle('active');
 }
 
 const navLink = document.querySelectorAll('.nav-links');
@@ -17,4 +21,6 @@ navLink.forEach((n) => n.addEventListener('click', closeMenu));
 function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  navLogo.classList.remove('active');
+  home.classList.remove('active');
 }
