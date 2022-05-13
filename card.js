c5944c8c8eb1d0b36c679012c67bf2283405e595
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-14 01:17:36 +0300

    Fix eslint

--- card.js
+++ card.js
@@ -1,13 +1,21 @@
-const cardTitle=['Website Protfolio ','Data DashboardHealthcare', 'Profesional Art Printing Data More ', 'Website Protfolio ','Data DashboardHealthcare',''];
-const cardPharagraph=`A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`;
+const cardTitle = [
+  "Website Protfolio ",
+  "Data DashboardHealthcare",
+  "Profesional Art Printing Data More ",
+  "Website Protfolio ",
+  "Data DashboardHealthcare",
+  "",
+];
+const cardPharagraph = `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`;
 console.log(cardPharagraph);
 
-const cardWrap=document.querySelector('.wrapper-desktop');
+const cardWrap = document.querySelector(".wrapper-desktop");
 
-for(let i=0;i<cardTitle.length;i++){
-    if(i<5){
-        const fatch=document.querySelector('.wrapper-desktop').innerHTML;
-        cardWrap.innerHTML=`<div class="pro-works${5-i}">
+for (let i = 0; i < cardTitle.length; i++) {
+  if (i < 5) {
+    const fatch = document.querySelector(".wrapper-desktop").innerHTML;
+    cardWrap.innerHTML =
+      `<div class="pro-works${5 - i}">
         <p class="website-portfolio-card">${cardTitle[i]}</p>
         <p class="website-portfolio-pharagraph">
           ${cardPharagraph}
@@ -34,9 +42,10 @@ for(let i=0;i<cardTitle.length;i++){
         </ul>
         <button class="cardbutton${i}">See Project</button>
       </div>` + fatch;
-    }
-    else{const fatch=document.querySelector('.wrapper-desktop').innerHTML;
-    cardWrap.innerHTML=`<div class="pro-works0">
+  } else {
+    const fatch = document.querySelector(".wrapper-desktop").innerHTML;
+    cardWrap.innerHTML =
+      `<div class="pro-works0">
     <p class="website-portfolio-card">${cardTitle[i]}</p>
     <p class="website-portfolio-pharagraph">
      jjjjj
@@ -62,12 +71,13 @@ for(let i=0;i<cardTitle.length;i++){
       </li>
     </ul>
     <button class="cardbutton${i}">See Project</button>
-  </div>` + fatch;}   
+  </div>` + fatch;
+  }
 }
 
-const multi = document.querySelector('.stories');
+const multi = document.querySelector(".stories");
 
-multi.innerHTML=`<img class="multi-desktop" src="image/multi.svg" alt="" />
+multi.innerHTML = `<img class="multi-desktop" src="image/multi.svg" alt="" />
 <img
   class="pharagraph-desktop"
   src="image/pargraph-desktop.png"
