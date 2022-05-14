d82d16fdbe991b8b89528253dfea786dfa964b7b
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-14 11:33:58 +0300

    Fix eslint

--- card.js
+++ card.js
@@ -1,19 +1,12 @@
 const cardTitle = [
-  'Website Protfolio ',
-  'Data DashboardHealthcare',
-  'Profesional Art Printing Data More ',
-  'Website Protfolio ',
-  'Data DashboardHealthcare',
-  '',
+  'Website Protfolio ','Data DashboardHealthcare','Profesional Art Printing Data More','Website Protfolio ','Data DashboardHealthcare','0'
 ];
-const cardPharagraph = 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard'
+const cardPharagraph = 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard';
 const cardWrap = document.querySelector('.wrapper-desktop');
-const me = document.createElement('div');
 
 for (let i = 0; i < cardTitle.length; i += 1) {
-  if (i < 5) {
-    cardWrap.innerHTML = `<div class="pro-works${5 - i}">
-        <p class="website-portfolio-card">${cardTitle[i]}</p>
+    cardWrap.innerHTML += `<div class="pro-works${i}">
+        <p class="website-portfolio-card">${cardTitle[5-i]}</p>
         <p class="website-portfolio-pharagraph">
           ${cardPharagraph}
         </p>
@@ -38,37 +31,7 @@ for (let i = 0; i < cardTitle.length; i += 1) {
           </li>
         </ul>
         <button class="cardbutton${i}">See Project</button>
-      </div>` + document.querySelector('.wrapper-desktop').innerHTML;
-  } else {
-    const fatch = document.querySelector('.wrapper-desktop').innerHTML;
-    cardWrap.innerHTML = `<div class='pro-works0'>
-    <p class='website-portfolio-card'>${cardTitle[i]}</p>
-    <p class='website-portfolio-pharagraph'>
-     jjjjj
-    </p>
-    <ul class='dashboard-list'>
-      <li>
-        <a href='#'
-          ><img class='dashbord-list-item' src='image/htmld.png' alt='image'
-        /></a>
-      </li>
-      <li>
-        <a href='#'
-          ><img
-            class='dashbord-list-item'
-            src='image/bootstrapd.png'
-            alt='image'
-        /></a>
-      </li>
-      <li>
-        <a href='#'
-          ><img class='dashbord-list-item' src='image/Rubyd.png' alt='image'
-        /></a>
-      </li>
-    </ul>
-    <button class='cardbutton${i}'>See Project</button>
-  </div>` + fatch;
-  }
+      </div>`;
 }
 
 const multi = document.querySelector('.stories');
